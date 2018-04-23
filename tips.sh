e311a4233de4a168f7a0ba2034c4cbf68a33f73a
abdias <abdias@alumnos.uaslp.edu.mx>
2018-04-22 22:27:58 -0500

    Update tips.sh

--- tips.sh
+++ tips.sh
@@ -7,11 +7,17 @@
 #git clone https://github.com/erikdubois/archopenbox
 #git clone https://github.com/palanthis/arch-openbox
 #nano /etc/pacman.conf
+#[archlabs_repo]
 #SigLevel = Optional TrustAll
+#Server = https://archlabs.github.io/archlabs_repo/$arch
+#Server = https://downloads.sourceforge.net/project/archlabs-repo/archlabs_repo/$arch
 
 #pacaur -S kickshaw ob-autostart xdg-user-dirs-gtk-update
 
 #polybar
+#libxfce4ui
+#libxfce4util
+#xfce4-settings
 #openbox key bindings
 #rofr.sh -r
 #jgmenu
